6194e08e4d9cf97f52ef94f6edaaff3866f47760
Jon Schulman <schulman99@gmail.com>
2018-06-26 19:30:31 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_selleciton = []
+  if collection.length == 0
+    puts "Nothing here but us bears."
+
+  else
+    while i < collection.length
+      if yield(collection[i])
+        new_selleciton << collection[i]
+      end
+    i = i+1
+    end
+  end
+ new_selleciton
 end
